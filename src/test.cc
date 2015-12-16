59219930783f0ab0bab5e50319d6e21bad56d438
MuraseTomohiro <mrs.tmhr@gmail.com>
2015-12-16 23:15:55 +0900

    modify test.cc and make .ignore

--- src/test.cc
+++ src/test.cc
@@ -6,20 +6,23 @@ int main() {
   int fail=0;
 
   /* test 1 */
-  sw::SmithWaterman smith_waterman("ACGT", "ACGT", 1, 1);
-  int score;
-  if ((score = smith_waterman.run()) != 4) {
-    cout << "ACGT - ACGT: " << score << " (correct: 4)" << endl;
-    ++ fail;
+  {
+    sw::SmithWaterman smith_waterman("ACGT", "ACGT", 1, 1);
+    int score;
+    if ((score = smith_waterman.run()) != 4) {
+      cout << "ACGT - ACGT: " << score << " (correct: 4)" << endl;
+      ++ fail;
+    }
   }
   /* test 2 */
-  sw::SmithWaterman smith_waterman("TGCA", "TGCA", 1, 1);
-  int score;
-  if ((score = smith_waterman.run()) != 4) {
-    cout << "TGCA - TGCA: " << score << " (correct: 4)" << endl;
-    ++ fail;
+  {
+    sw::SmithWaterman smith_waterman("TGCA", "TGCA", 1, 1);
+    int score;
+    if ((score = smith_waterman.run()) != 4) {
+      cout << "TGCA - TGCA: " << score << " (correct: 4)" << endl;
+      ++ fail;
+    }
   }
-
   cout << fail << " test failed." << endl;
   if (0 < fail) {return 1;}
   return 0;
